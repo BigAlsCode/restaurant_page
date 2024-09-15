7b1daa8c35b5a2429e6153c7241bfa1b25bc9d83
FatAnorexic <54606095+FatAnorexic@users.noreply.github.com>
2024-09-15 14:50:19 -0400

    remove test styling and image
    The console log is left alone for now to ensure the page is up and running
    for every instance of changing the html and css

--- src/index.js
+++ src/index.js
@@ -1,9 +1,4 @@
 import "./styles.css"
-import testImage from "./assets/testRemove.png"
 
-const image=document.createElement('img');
-image.src=testImage;
 console.log("The page is up and running")
 
-const content=document.getElementById("content")
-content.appendChild(image)
