d09f37d6ed6ec8284dd414593ee9fc97b591e521
FatAnorexic <54606095+FatAnorexic@users.noreply.github.com>
2024-09-15 14:18:56 -0400

    Add image loading to module bundle and test

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
 import "./styles.css"
+import testImage from "./assets/Designer.png"
+
+const image=document.createElement('img');
+image.src=testImage;
 console.log("The page is up and running")
+
+const content=document.getElementById("content")
+content.appendChild(image)
