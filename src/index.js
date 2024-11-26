56d602be14f30ba495af616be26a1d1fd5dfd66c
FatAnorexic <54606095+FatAnorexic@users.noreply.github.com>
2024-11-26 13:58:19 -0500

    Finishing adding beers menu, and test implementation

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import "./styles.css"
 import {homeSplash, homeMessage} from "./home.js";
+import { alcohol } from "./menu.js";
 
 console.log("The page is up and running")
 
 let content=document.getElementById('content')
 
 content.appendChild(homeSplash());
-content.appendChild(homeMessage())
+content.appendChild(homeMessage());
+content.appendChild(alcohol());
--- src/menu.js
+++ src/menu.js
@@ -11,4 +11,26 @@ function alcohol(){
     let beerList=["FATHEAD'S-HEAD HUNTER IPA", "TROEGS-PERPETUAL IPA", "ARSENAL-PUMPKIN CIDER", 
                   "GUINESS-STOUT", "SOUTHERN TIER-PUMKING", "MILLER LITE-LAGER", "YUENGLING LAGER",
                   "TROEG'S HOPRA IPA 12oz CANS", "BEER OF THE MONTH $4.5"];
+
+    const beers=document.createElement('div');
+    beers.className='beers';
+    const headBeer=document.createElement('h1');
+    headBeer.className='seasonal';
+    headBeer.textContent='Seasonal Beer List';
+    beers.appendChild(headBeer);
+
+    for(let beer of beerList){
+        const head=document.createElement('h2');
+        const span=document.createElement('span');
+        span.className='highlight';
+        span.textContent=beer;
+        head.appendChild(span);
+        beers.appendChild(head);
+    }
+
+    boozeMen.appendChild(beers);
+    booze.appendChild(boozeMen);
+    return booze;
 }
+
+export {alcohol}
