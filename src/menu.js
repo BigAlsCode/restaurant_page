90f03fa5ca068981c9af8254007913a2eec9cc7e
FatAnorexic <54606095+FatAnorexic@users.noreply.github.com>
2024-11-26 07:34:23 -0500

    Add alcohol function to display booze menu
    May need to use a factory function or a for loop to iterate and display all booze names.

---
+++ src/menu.js
@@ -0,0 +1,10 @@
+import dinMen from "./assets/menu2edit.png"
+import brkMen from "./assets/breakfast2.png"
+
+function alcohol(){
+    const booze=document.createElement('div');
+    const boozeMen=document.createElement('div');
+    booze.className='beersImage';
+    boozeMen.className='alcohol';
+    boozeMen.id='alcohol';
+}
