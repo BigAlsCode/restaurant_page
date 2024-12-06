c3ab88f27f97fb9a9d51859753627c9147fcd0a0
FatAnorexic <54606095+FatAnorexic@users.noreply.github.com>
2024-12-06 00:28:18 -0500

    Add drink description array

--- src/menu.js
+++ src/menu.js
@@ -12,6 +12,23 @@ function alcohol(){
                   "GUINESS-STOUT", "SOUTHERN TIER-PUMKING", "MILLER LITE-LAGER", "YUENGLING LAGER",
                   "TROEG'S HOPRA IPA 12oz CANS", "BEER OF THE MONTH $4.5"];
 
+    let drinkNames=['bloody mary', 'cappys cosmo', 'moscow mule', 
+                    'john daly', 'dark and stormy', 'mangaria', 
+                    'lavender taquini', 'big apple', 'elderflower fizz'];
+
+    let drinkDesc=[
+        "Tito's Vodka, secret house recipe, with or without horseradish, served with lemon",
+        "Smirnoff Rasberry Vodka, Cointreau, Rose's Sweet Lime, dash of bitters, served in a copper mug",
+        "Tito’s vodka, Jamaica’s Finest ginger beer (NA), Rose’s sweet lime, dash of bitters, served in a copper mug.",
+        "Absolut Citron, lemonade, unsweetened iced tea, served in a tall glass.",
+        "Meyer’s dark rum, Jamaica’s Finest ginger beer (NA), Rose’s sweet lime, served in a copper mug.",
+        "Frontera Cabernet-Merlot blend, house vodka, triple sec, orange juice, grenadine, Rose’s sweet lime.",
+        "Hornitos Plata tequila, St. Germain, Chambord, sours mix, served up.",
+        "CROWN ROYAL APPLE, Sweet and Dry Vermouth, bitters, served up or on the rocks with a cinnamon stick",
+        "Hendricks Gin, St. Germain, soda, fresh lemon."
+    ];
+    
+
     const beers=document.createElement('div');
     beers.className='beers';
     const headBeer=document.createElement('h1');
@@ -27,6 +44,9 @@ function alcohol(){
         head.appendChild(span);
         beers.appendChild(head);
     }
+
+    const drinks=document.createElement('div');
+    drinks.className='drinks';
 
     boozeMen.appendChild(beers);
     booze.appendChild(boozeMen);
