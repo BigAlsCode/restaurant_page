7ce8b4cc18688b10adab2a0437b90d07afb0bc5e
FatAnorexic <54606095+FatAnorexic@users.noreply.github.com>
2025-01-19 22:23:55 -0500

    Loads each page independent of the other| defaults home

--- src/index.js
+++ src/index.js
@@ -1,21 +1,40 @@
 import "./styles.css"
 import {homeSplash, homeMessage} from "./home.js";
 import { alcohol, dinner, breakfast, events, contact} from "./menu.js";
-
-
-
-
-
 console.log("The page is up and running")
 
-let content=document.getElementById('content')
+const buttons=document.querySelectorAll('button');
+const content=document.getElementById('content');
 
-content.appendChild(homeSplash());
-content.appendChild(homeMessage());
-content.appendChild(alcohol());
-content.appendChild(dinner());
-content.appendChild(breakfast());
-content.appendChild(events());
-content.appendChild(contact());
+(()=>{
+    content.appendChild(homeSplash());
+    content.appendChild(homeMessage());
+    content.appendChild(contact());
+})(content);
 
 // Loads the webpage clicked on
+buttons.forEach(button=>{
+    button.addEventListener('click',()=>{
+        console.log(button.className);
+        
+        content.replaceChildren();
+        switch (button.className){
+            case 'menu':
+                content.appendChild(alcohol());
+                content.appendChild(dinner());
+                content.appendChild(breakfast());
+                content.appendChild(contact());
+                break;
+            case 'specialEvents':
+                content.appendChild(events());
+                content.appendChild(contact());
+                break;
+            default:
+                content.appendChild(homeSplash());
+                content.appendChild(homeMessage());
+                content.appendChild(contact());
+                break;
+        }
+    });
+});
+
