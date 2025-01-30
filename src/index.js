b96fa2899cdd70bb3de175dcc2bd3975f2a0d7a1
FatAnorexic <54606095+FatAnorexic@users.noreply.github.com>
2025-01-30 00:05:00 -0500

    Computer dying, commit

--- src/index.js
+++ src/index.js
@@ -1,42 +1,46 @@
 import "./styles.css"
 import {homeSplash, homeMessage} from "./home.js";
 import { alcohol, dinner, breakfast, events, contact} from "./menu.js";
-console.log("The page is up and running")
+console.log("The page is up and running");
 
-const buttons=document.querySelectorAll('button');
+// const buttons=document.querySelectorAll('button');
 const content=document.getElementById('content');
-
-
-
+const menuDrop=document.getElementById('menu');
 
 (()=>{
     content.appendChild(homeSplash());
     content.appendChild(homeMessage());
     content.appendChild(contact());
-})(content);
+})();
+
+menuDrop.addEventListener('click',()=>{
+    document.getElementById('myDropdown').classList.add('show')
+});
+
+
 
 // Loads the webpage clicked on
-buttons.forEach(button=>{
-    button.addEventListener('click',()=>{
-        console.log(button.className);
+// buttons.forEach(button=>{
+//     button.addEventListener('click',()=>{
+//         console.log(button.className);
         
-        content.replaceChildren();
-        switch (button.className){
-            case 'menu':
-                content.appendChild(alcohol());
-                content.appendChild(dinner());
-                content.appendChild(breakfast());
-                content.appendChild(contact());
-                break;
-            case 'specialEvents':
-                content.appendChild(events());
-                content.appendChild(contact());
-                break;
-            default:
-                content.appendChild(homeSplash());
-                content.appendChild(homeMessage());
-                content.appendChild(contact());
-                break;
-        }
-    });
-});
+//         content.replaceChildren();
+//         switch (button.className){
+//             case 'menu':
+//                 content.appendChild(alcohol());
+//                 content.appendChild(dinner());
+//                 content.appendChild(breakfast());
+//                 content.appendChild(contact());
+//                 break;
+//             case 'specialEvents':
+//                 content.appendChild(events());
+//                 content.appendChild(contact());
+//                 break;
+//             default:
+//                 content.appendChild(homeSplash());
+//                 content.appendChild(homeMessage());
+//                 content.appendChild(contact());
+//                 break;
+//         }
+//     });
+// });
