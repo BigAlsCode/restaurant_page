bda009a0563af161db15ae58a30454bf8658b541
FatAnorexic <54606095+FatAnorexic@users.noreply.github.com>
2025-02-04 12:14:20 -0500

    Link to all pages and add scroll events
    This commit not only adds all the proper loading of pages, but also
    ensures that when clicked, the page is returned to the top. This
    change was made because clicking the contact link scrolls to the
    bottom of the page, and when loading a different page would
    remain in place. The reloading of the page still needs to
    be fixed such that it sticks to the current page.

--- src/index.js
+++ src/index.js
@@ -30,10 +30,50 @@ window.addEventListener('click', (e)=>{
     }
 });
 
+// Function to determine what page to load
+function loadPage(link){
+    // scroll to the top of the page if not already there
+    window.scrollTo({top:0, behavior:'smooth'});
+
+    // executes the functions of menu.js and home.js
+    // based on the id given in the link argument
+    switch(link.id){
+        case 'specialEvents':
+            content.replaceChildren();
+            content.appendChild(events());
+            break;
+
+        case 'drinks':
+            content.replaceChildren();
+            content.appendChild(alcohol());
+            break;
+
+        case 'dinner':
+            content.replaceChildren();
+            content.appendChild(dinner());
+            break;
+
+        case 'breakfast':
+            content.replaceChildren();
+            content.appendChild(breakfast());
+            break;
+
+        case 'about':
+            document.querySelector('footer').scrollIntoView({behavior:'smooth'});
+            break;
+
+        default:
+            content.replaceChildren();
+            content.appendChild(homeSplash());
+            content.appendChild(homeMessage());
+            break;
+    }
+    content.appendChild(contact());
+}
 
 // Loads the webpage clicked on
 links.forEach(link=>{
     link.addEventListener('click',()=>{
-        console.log(link.id)
+        loadPage(link);
     });
 });
