c89ef20b2425ddb3dc562236e69474b7c4196538
FatAnorexic <54606095+FatAnorexic@users.noreply.github.com>
2025-02-04 11:31:18 -0500

    Make mouse a pointer when hovering over buttons

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import "./styles.css"
 import {homeSplash, homeMessage} from "./home.js";
 import { alcohol, dinner, breakfast, events, contact} from "./menu.js";
-console.log("The page is up and running");
 
-const buttons=document.querySelectorAll('.direct');
+console.log("The page is up and running");
+// Main div container for each page
 const content=document.getElementById('content');
+
+const links=document.querySelectorAll('.direct')
 const menuDropDown=document.querySelector('.menu');
 const dropDownCont=document.querySelector('.dropdown__menu');
 
@@ -30,27 +32,8 @@ window.addEventListener('click', (e)=>{
 
 
 // Loads the webpage clicked on
-buttons.forEach(button=>{
-    button.addEventListener('click',()=>{
-        console.log(button.className);
-        
-        content.replaceChildren();
-        switch (button.className){
-            case 'menu':
-                content.appendChild(alcohol());
-                content.appendChild(dinner());
-                content.appendChild(breakfast());
-                content.appendChild(contact());
-                break;
-            case 'specialEvents':
-                content.appendChild(events());
-                content.appendChild(contact());
-                break;
-            default:
-                content.appendChild(homeSplash());
-                content.appendChild(homeMessage());
-                content.appendChild(contact());
-                break;
-        }
+links.forEach(link=>{
+    link.addEventListener('click',()=>{
+        console.log(link.className);
     });
 });
