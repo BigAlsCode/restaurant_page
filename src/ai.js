5ca3f0b4870794ed5c801f6c44711fa8e14d72a6
FatAnorexic <54606095+FatAnorexic@users.noreply.github.com>
2025-03-22 15:35:00 -0400

    Create coming soon text content for AI page

--- src/ai.js
+++ src/ai.js
@@ -1,6 +1,14 @@
 
 export default function ai(){
     const aiPage=document.createElement('div');
+    const aiComingSoon=document.createElement('h1');
+
+    aiPage.className='aiMain';
+    aiComingSoon.className='comingSoon';
+
+    aiComingSoon.textContent='Coming Soon!'
+
+    aiPage.appendChild(aiComingSoon);
 
     return aiPage;
 }
